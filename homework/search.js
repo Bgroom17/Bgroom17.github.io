a5f952ded75d4d5208d97622a045a75db1bad0fa
Bgroom17.github.io <gro15010@byui.edu>
2016-12-03 13:43:18 -0700

    edits

--- homework/search.js
+++ homework/search.js
@@ -1,64 +1,10 @@
-//var returned; 
-//$('#query').keyup(function () {
-//             var value = $('#query').val();
-//             var rExp = new RegExp(value, "i");
-//             $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
-//                 console.log(data);
-//                 //Begin building output
-//                 var output = '<ol>';
-//                 var i = 0;
-//                 $.each(data.RESULTS, function (key, val) {
-//                     if (val.name.search(rExp) != -1) {
-//                         output += '<li>';
-//                         output += '<a href = "https://www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
-//                         output += '</li>';
-//                     }
-//                 });
-//                 // end each
-//                 output += '</ol>';
-//                 
-//                 $("#searchResults").html(output); //send results to the page
-//             }); //end getJason
-//
-//
-//
-//             // Get weather data from wunderground.com
-//             function getData(input) {
-//                 // Get the data from the wunderground API
-//                  $.ajax({
-//                                 url: "https://api.wunderground.com/api/3f747b309a7431f8/geolookup/conditions/forecast/q/" + input + ".json",
-//                                 dataType: "jsonp",
-//                                 success: function (data) {
-//                                     console.log(data);
-//                                     var location = data.location.city + ', ' + data.location.state;
-//                                     var temp_f = data.current_observation.temp_f;
-//                                     console.log('Location is: ' + location);
-//                                     console.log('Temp is: ' + temp_f);
-//                                     $("#cityDisplay").text(location);
-//                                     $("title").html(location + " | Weather Center");
-//                                     $("#currentTemp").html(Math.round(temp_f) + '°');
-//                                     $("#summary").text(toTitleCase(data.current_observation.icon));
-//                                     $("#cover").fadeOut(250);
-//                                     document.getElementById(ol).style.display = none;
-//                                 }
-//                             });}
-//                 // Intercept the menu link clicks
-//                 $("#searchResults").on("click", "a", function (evt) {
-//                             evt.preventDefault();
-//                             // With the text value get the needed value from the weather.json file
-//                             var jsonCity = $(this).text(); // Franklin, etc...
-//                             console.log(jsonCity);
-//                            index= $(this).index("a");
-//                            getData(returned.RESULTS[index].zmw);
-//                            }
-
 var returned;
 
 $('#query').keyup(function(){
   // All code will be inside of this block
   var value = $('#query').val();
   var rExp = new RegExp(value, "i");
-  $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+$.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
     console.log(data);
     returned = data;
 
@@ -78,7 +24,7 @@ $('#query').keyup(function(){
 
 
   // Intercept the menu link clicks
-  $("searchResults").on("click", "a", function(evt) {
+  $("#searchResults").on("click", "a", function(evt) {
     evt.preventDefault();
 
     // With the text value get the needed value from the weather.json file
@@ -88,6 +34,7 @@ $('#query').keyup(function(){
     console.log(index);
     console.log(returned);
     getData(returned.RESULTS[index].lat, returned.RESULTS[index].lon);
+document.getElementById(searchResults).style.display = none;
 
 }); // end keyup
 });
@@ -108,13 +55,14 @@ function getData(lat, long) {
       var visibility = data.current_observation.visibility_mi;
       $('#h1').text(location);
       $('title').text(location + ' | Weather Home');
-      $('#currenttemp').text(temp + "°F");
+      $('#currentTemp').text(temp + "°F");
       $('#high').text(high + "°F");
       $('#low').text(low + "°F");
       $('#summary').text(summary);
-      $('#feelslike').text("Feels Like: " + feelsLike + "°F");
-      $('#windspeed').text("Windspeed: " + windspeed + " mph");
-      $('#visibility').text("Visibility: " + visibility + " mi");
+      $('#add1').text("Feels Like: " + feelsLike + "°F");
+      $('#add2').text("Windspeed: " + windspeed + " mph");
+      $('#add3').text("Visibility: " + visibility + " mi");
+    
     }
   });
 }
---
+++ photography-portfolio/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("#page-header").load("/photography-portfolio/header.html");
+    $("#page-nav").load("/photography-portfolio/nav.html");
+$("#page-footer").load("/photography-portfolio/footer.html");
+});
