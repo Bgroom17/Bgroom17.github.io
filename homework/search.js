439ccb50953168a1c819328ccfce0b37dfc6856b
Bgroom17.github.io <gro15010@byui.edu>
2016-12-05 11:26:53 -0700

    fixed!

--- homework/search.js
+++ homework/search.js
@@ -1,43 +1,42 @@
 
-
-$('#query').keyup(function () {
-    // All code will be inside of this block
-    var value = $('#query').val();
+ $('#query').keyup(function(){
+    var value = $('#query').val(); 
     var rExp = new RegExp(value, "i");
-    $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
-        console.log(data);
-        returned = data;
+     $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+           //console.log(data);
+         returned = data;
+         //Begin building output
+         var output = '<ol>';
+         $.each(data.RESULTS, function(key, val){
+             if (val.name.search(rExp) != -1){
+                 output += '<li>';
+                 output += '<a href = "#" onclick ="getData('+val.lat+','+val.lon+')" title="See results for ' + val.name + '">' + val.name + '</a>' ;
+                 output += '</li>';
+             }
+         });
+         // end each
+         output += '</ol>';
+         $("#searchResults").html(output); //send results to the page
+     });//end getJason
+     
+      
 
-        // Begin building output
-        var output = '<ol>';
-        $.each(data.RESULTS, function (key, val) {
-            if (val.name.search(rExp) != -1) {
-                output += '<li>';
-                output += '<a href="https://api.wunderground.com/api/3f747b309a7431f8/geolookup/conditions/forecast/q/' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
-                output += '</li>';
-            }
-        }); // end each
-        output += '</ol>';
-        $("#searchResults").html(output); // send results to the page
-    }); // end getJSON
 
+//    // Intercept the menu link clicks
+//    $("#searchResults").on("click", "a", function (evt) {
+//        evt.preventDefault();
+//        var jsonCity = $(this).text(); // Franklin, etc...
+//        //console.log(jsonCity);
+//        index = $(this).index("#searchResults a");
+//        //console.log(index);
+//        //console.log(returned);
+//        getData(returned.RESULTS[index].lat , returned.RESULTS[index].long);
+//        $("#searchResults").html("");
 
+//    }); // end keyup
+ });
 
-    // Intercept the menu link clicks
-    $("#searchResults").on("click", "a", function (evt) {
-        evt.preventDefault();
-        var jsonCity = $(this).text(); // Franklin, etc...
-        console.log(jsonCity);
-        index = $(this).index("#searchResults a");
-        console.log(index);
-        console.log(returned);
-        getData(returned.RESULTS[index].lat, returned.RESULTS[index].lon);
-        $("#searchResults").html("");
-
-    }); // end keyup
-});
-
-function getData(lat, long) {
+function getData(lat, long) {$("#searchResults").html("");
     $.ajax({
         url: 'https://api.wunderground.com/api/3f747b309a7431f8/geolookup/conditions/forecast/q/' + lat + ',' + long + '.json',
         dataType: "jsonp",
@@ -45,27 +44,29 @@ function getData(lat, long) {
             console.log(data);
             var location = data['location']['city'] + ', ' + data['location']['state'];
             var temp = Math.round(data['current_observation']['temp_f']);
-            var high = Math.round(data['forecast']['simpleforecast']['forecastday']['0']['high']['fahrenheit'];
-            var low = Math.round(data['forecast']['simpleforecast']['forecastday']['0']['low']['fahrenheit'];
-            var summary = data['current_observation']['weather'];
-            var feelsLike = data['current_observation']['feelslike_f'];
-            var windspeed = data['current_observation']['wind_mph'];
-            var visibility = data['current_observation']['visibility_mi'];
+            var high = Math.round(data['forecast']['simpleforecast']['forecastday']['0']['high']['fahrenheit']);
+            var low = Math.round(data['forecast']['simpleforecast']['forecastday']['0']['low']['fahrenheit']);
+            var summary = (data['current_observation']['weather']);
+            var feelsLike = (data['current_observation']['feelslike_f']);
+            var windspeed = (data['current_observation']['wind_mph']);
+            var visibility = (data['current_observation']['visibility_mi']);
             $('#h1').html(location);
             $('title').html(location + ' | Weather Home');
-            $('#currentTemp').html(temp + "°F");
-            $('#high').html(high + "°F");
-            $('#low').html(low + "°F");
+            $('#currentTemp').html(temp + "&#176;F");
+            $('#high').html("High: "+(high)+ "&#176;F");
+            $('#low').html("Low: "+(low) + "&#176;F");
             $('#summary').html(summary);
-            $('#feelslike').html("Feels Like: " + feelsLike + "°F");
+            $('#feelslike').html("Feels Like: " + feelsLike + "&#176;F");
             $('#windspeed').html("Windspeed: " + windspeed + " mph");
             $('#visibility').html("Visibility: " + visibility + " mi");
+            $('#cover').fadeOut(250);
      
-
-
-// A function for changing a string to TitleCase
-function toTitleCase(str) {
-    return str.replace(/\w+/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
     });
 };
+
+// A function for changing a string to TitleCase
+//function toTitleCase(str) {
+//    return str.replace(/\w+/g, function (txt) {
+ //       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+ //   });
