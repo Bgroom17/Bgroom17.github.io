f868bcf6c1658ce32fa0caca035676c6d438a4b6
Bgroom17.github.io <gro15010@byui.edu>
2016-12-09 17:37:35 -0700

    updates

--- photography-portfolio/fine-art.js
+++ photography-portfolio/fine-art.js
@@ -1,34 +1,47 @@
- // Get the data from the wunderground API
-//  $.getJSON( "ajax/photography-portfolio/fine.json", 
-            $(function() {
-       console.log("test");
-        $.ajax({
-            url: '/photography-portfolio/fine.json',
-            dataType: "jsonp",
-            success: function (data) {
-                console.log(data);
-                var bridge = data['fine-art']['bridge'];
-                var cart = data['fine-art']['cart'];
-                var close = data['fine-art']['close-fence'];
-                var fence = data['fine-art']['fence'];
-                var wheel = data['fine-art']['wheel'];
-                var skull = data['fine-art']['skull'];
-                var tools = data['fine-art']['tools'];
-                var wheat = data['fine-art']['wheat'];
-                var leaf = data['fine-art']['red-leaf'];
-        
-                    
-                $('#bridge').html(bridge);
-                $('#cart').html(cart);
-                $('#close-fence').html(close);
-                $('#fence').html(fence);
-                $('#wheel').html(wheel);
-                $('#skull').html(skull);
-                $('#tools').html(tools);
-                $('#wheat').html(wheat);
-                $('#leaf').html(red-leaf);
-                
-                
+// // Get the data from the wunderground API
+////  $.getJSON( "ajax/photography-portfolio/fine.json", 
+//            $(function() {
+//       console.log("test");
+//        $.ajax({
+//            url: '/photography-portfolio/fine.json',
+//            dataType: "json",
+//            success: function (data) {
+//                console.log(data);
+//                var bridge = data.fine.bridge;
+//                var cart = data.fine['cart'];
+//                var close = data.fine['close-fence'];
+//                var fence = data.fine['fence'];
+//                var wheel = data.fine['wheel'];
+//                var skull = data.fine['skull'];
+//                var tools = data.fine['tools'];
+//                var wheat = data.fine['wheat'];
+//                var leaf = data.fine['red-leaf'];
+//        console.log(bridge);
+//                    
+//                $('#bridge').text(bridge);
+//                $('#cart').text(cart);
+//                $('#close-fence').text(close);
+//                $('#fence').text(fence);
+//                $('#wheel').text(wheel);
+//                $('#skull').text(skull);
+//                $('#tools').text(tools);
+//                $('#wheat').text(wheat);
+//                $('#leaf').text(leaf);
+//                
+//                
+//
+//            }
+//        });});
 
-            }
-        });});
+$.getJSON( "fine.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li> <img src='"+val+ "' alt='" +key+ "'></li>");
+    
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "#test" );
+});
