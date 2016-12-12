2c434349b5357fe6e303706a9f0cbd68ceaba643
Bgroom17.github.io <gro15010@byui.edu>
2016-12-12 11:23:30 -0700

    homework folder

---
+++ homework/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("#page-header").load("/homework/header.html");
+    $("#page-nav").load("/homework/nav.html");
+$("#page-footer").load("/homework/footer.html");
+});
--- modules.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    $("#page-header").load("/header.html");
-    $("#page-nav").load("/nav.html");
-$("#page-footer").load("/footer.html");
-});
